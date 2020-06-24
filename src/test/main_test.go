9cf715e734679c3ed4bce072e565ee76680f0519
Emerson Hinterholz <emersonhinterholz@gmail.com>
2020-06-24 20:51:41 -0300

    directories organization

--- src/test/main_test.go
+++ src/test/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"../utils"
 )
 
 func TestSum(t *testing.T) {
@@ -16,7 +18,7 @@ func TestSum(t *testing.T) {
 		{x: 2, y: 4, want: 6},
 	}
 	for _, tt := range tests {
-		if got := Sum(tt.x, tt.y); got != tt.want {
+		if got := utils.Sum(tt.x, tt.y); got != tt.want {
 			t.Errorf("sum(%v, %v) = %v, want %v", tt.x, tt.y, got, tt.want)
 		}
 	}
---
+++ src/utils/sum.go
@@ -0,0 +1,6 @@
+package utils
+
+/*Sum is the sum function exported*/
+func Sum(x int, y int) int {
+	return x + y
+}
